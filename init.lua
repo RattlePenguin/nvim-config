b632c662639d7ac790299ae18b6d11eb2e8a3f8d
RattlePenguin <viggo.setiawan@gmail.com>
2026-01-13 16:22:49 +0800

    Added existing basic nvim config, inspo by poltora blog

---
+++ init.lua
@@ -0,0 +1,50 @@
+local vim = vim
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+
+-- theme
+Plug('rebelot/kanagawa.nvim')
+
+-- vimtree
+Plug('kyazdani42/nvim-tree.lua')
+Plug('kyazdani42/nvim-web-devicons')
+
+-- tab bars
+Plug('romgrk/barbar.nvim')
+
+-- status line
+Plug('nvim-lualine/lualine.nvim')
+
+-- syntax tree or something, helps with syntax highlighting
+Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+
+-- LSP, autocomplete
+Plug('neovim/nvim-lspconfig')
+
+-- something
+Plug('hrsh7th/cmp-buffer') 
+Plug('hrsh7th/cmp-path')
+Plug('hrsh7th/cmp-cmdline') 
+Plug('hrsh7th/nvim-cmp')
+Plug('hrsh7th/cmp-nvim-lsp') 
+Plug('hrsh7th/cmp-nvim-lsp-signature-help')
+
+vim.call('plug#end')
+
+home = os.getenv("HOME")
+package.path = home .. "/.config/nvim/?.lua;" .. package.path
+
+require "common"
+require "theme"
+require "vimtree"
+require "barbar"
+require "lua_line"
+require "lsp"
+require "cmp_config"
+
+-- LSP enable configurations
+vim.lsp.enable('gopls')
+
+-- command line abbreviations
+vim.keymap.set("ca", "tree", "NvimTreeOpen")
---
+++ lsp.lua
@@ -0,0 +1,47 @@
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
+
+-- Taken from vpoltora/go-rust-nvim-config
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true }),
+  callback = function(ev)
+    local bufnr = ev.buf
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
+    
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set('n', '<space>wl', function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+    vim.keymap.set('n', '<space>f', function()
+      vim.lsp.buf.format({ async = false })
+    end, bufopts)
+    
+    if client then
+      print(string.format('✓ LSP attached: %s (buffer %d)', client.name, bufnr))
+    end
+  end,
+})
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+	vim.lsp.diagnostic.on_publish_diagnostics, {
+		virtual_text = false
+	}
+)
